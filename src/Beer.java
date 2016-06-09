8232a107acd7d79bfb30a3aa097698ba8eea9359
jonathandavidblack <jonnybl915@gmail.com>
2016-06-09 13:36:28 -0400

    add Beer class

---
+++ src/Beer.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jonathandavidblack on 6/9/16.
+ */
+public class Beer {
+    String beerName;
+    String breweryName;
+    String beerStyle;
+    int abv;
+    int ibu;
+    String comment;
+
+    public Beer(String beerName, String breweryName, String beerStyle, int abv, int ibu, String comment) {
+        this.beerName = beerName;
+        this.breweryName = breweryName;
+        this.beerStyle = beerStyle;
+        this.abv = abv;
+        this.ibu = ibu;
+        this.comment = comment;
+    }
+}
